ea3b4b6ac77c0fea93bde8df4c8cc5702e68e7ed
MicleGulin <m-gulin@ya.ru>
2021-04-19 02:07:39 +0300

    Commit 3

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-﻿from flask import Flask
+﻿import os
+
+from flask import Flask
 from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
@@ -11,4 +13,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
