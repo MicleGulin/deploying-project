6f7503f1585d21f015176da74db7c53b13032363
admin <m-gulin@ya.ru>
2022-03-27 01:06:07 +0300

    Commit 6

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask"
+    return "Привет от Гулина Михаила"
 
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
-# Еще коммент
